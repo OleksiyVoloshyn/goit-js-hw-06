c9c84930e074702e75a453f0f894b59355fadd23
OleksiyGromov <gromov196312@gmail.com>
2024-10-06 23:29:18 +0300

    task2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,30 @@
 'use strict'
+class Storage {
+    
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItems) {
+        return this.#items.push(newItems);
+    }
+
+    removeItem(itemToRemove) {
+        return this.#items = this.#items.filter(el => el !== itemToRemove);
+    }
+}
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
